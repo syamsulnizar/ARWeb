62d0dc5060069c5cf4fada54b646fcdd849eefb3
syamsulnizar <syamsulnizar@student.telkomuniversity.ac.id>
2021-03-08 12:25:41 +0700

    WebAR

---
+++ Assets/UrlOpener.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class UrlOpener : MonoBehaviour
+{
+
+    public string Url;
+
+
+    public void Open()
+    {
+        Application.OpenURL(Url);
+    }
+}    
---
+++ Assets/close.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class close : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Canvas start;
+    public Button exit;
+
+    public void keluar()
+    {
+        Application.Quit();
+    }
+}
